d26462575db67dacae4ab48975d6a7dabb1dda2c
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-06-22 23:16:55 +0530

    Added Icons.

--- script.js
+++ script.js
@@ -11,16 +11,19 @@ export default async function renderProfile() {
   const user = input.value;
   const userDetails = await getProfile(user);
   console.log(userDetails);
-  card.innerHTML = `<div  class="w-1/2 h-3/4 my-10  mx-auto flex items-center gap-10  bg-[#4f4f4f] py-5  shadow-sm shadow-black" >
+  card.innerHTML = `<div  class="w-1/2 h-3/4 my-10  mx-auto flex items-center gap-10  bg-[#4f4f4f] py-5  shadow-sm shadow-black relative" >
             <div class="border-2 rounded-full w-[200px] overflow-hidden mx-auto">
               <img src=${userDetails.avatar_url} alt="" >
             </div>
-            <div class="mx-auto text-white  ">
+            <div class="mx-auto text-white">
               <p class="font-mono font-semibold ">Name: ${userDetails.name}</p>
               <p>Repos: ${userDetails.public_repos}</p>
-              <p class="break-words"><a href=${userDetails.html_url} target="_blank">Url: ${userDetails.html_url}</a></p>
+              <p class="break-words"><a href=${userDetails.html_url} target="_blank"><ion-icon class="text-2xl align-middle" name="link-outline"></ion-icon>${userDetails.html_url}</a></p>
             </div>
-          </div>
+            <div class="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-white">
+            ID: ${userDetails.id}
+            </div>
+            </div>
           `;
   /* 
   card.innerHTML = ` <div class="left-section">
